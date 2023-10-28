712468b3ed95a8c1e8fc930d07c2c64168a1ebb0
ElinaBoman <>
2023-10-28 12:03:01 +0000

    Added first function to collect number of walks from user.

--- run.py
+++ run.py
@@ -13,8 +13,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Lucias_dog_walk_AB')
 
-walks = SHEET.worksheet('walks')
 
-data = walks.get_all_values()
+def get_number_of_walks():
+    """
+    Get number of walks input from the user
+    """
+    print('Please enter number of walks for each dog')
+    print('Data should be separated by commas')
+    print('Each number represents total walks for one dog in a day')
+    print('Example: 1, 2, 3, 4')
 
-print(data)
+    data_str = input('Enter number of walks here: ')
+    print(f'Number of walks provides is {data_str}')
+
+
+get_number_of_walks()
